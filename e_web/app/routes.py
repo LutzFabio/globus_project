ba2a2998c8bf7960a0fe2e56be7121a8a144c81a
maffka123 <mstihomirova@gmail.com>
2019-11-22 16:44:50 +0100

    comment update

--- e_web/app/routes.py
+++ e_web/app/routes.py
@@ -1,13 +1,15 @@
+'''
+Main function of the web app, has all routing directions and classification function
+'''
 
+#----------------------------Imports
 from __future__ import absolute_import, division, print_function, unicode_literals
 from flask import Flask
 import pandas as pd
 import numpy as np
 import itertools
-import matplotlib.pyplot as plt
 import copy
 import tensorflow as tf
-#import keras
 from flask import render_template, url_for, request, redirect, flash, session
 from app import app
 from werkzeug.utils import secure_filename
@@ -17,43 +19,53 @@ from keras.applications.resnet50 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from PIL import Image
 
-IMAGE_DIMS=(224,224,3)
-
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
+#--------------------------Some variables
+IMAGE_DIMS=(224,224,3) #target dimentions for ResNet50 model
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'} #still did not implement it, need to restrict only to upload images
 img=[]
-
+#Find all images already in app
 for f in os.listdir("app/static/images"):
     if f.endswith('.png') or f.endswith('.jpg') or f.endswith('.webp'):
         img.append(f)
 
-#Forwar all requests to page with image name
+#############################START - ROOT / ##################################
+#Forward all requests to page with image name
 @app.route('/')
 def index():
+    '''
+    re-roots any request to the page with image name
+    '''
     start_img=img[0]
     return redirect('/'+start_img)
 
+#############################IMEGE DISPLAY##################################
 #Main page with all stuff
 @app.route('/<string:next_img>', methods=['GET', 'POST'])
 def update_img(next_img):
+    '''
+    Main page shows cassification for image
+    input: image to show
+    output: rendered index.html
+    '''
 
+    # Set default model
     used_model='ResNet50_2layers.h5'
+    # Check if there was some other model selected
     if request.method == 'POST':
         select = request.form.get('used_model') 
         if select is not None:
             used_model=select
             session['used_model']=select
     if 'used_model' in session:
-        used_model=session['used_model']
+        used_model=session['used_model'] 
 
-    
-
-    #Classify/load predictions
-
+    # Load predictions if they already exists or create new df to keep what was already classified
     if os.path.exists("app/static/cache/model_result.csv"):
         df=pd.read_pickle('app/static/cache/model_result.csv')
     else:
         df=pd.DataFrame(columns=['model','image','final_dict', 'feature_list', 'rest_features','p_color','col_s'])
 
+    # Check if classification already exist in df or calculate a new one    
     if (next_img not in df['image'].values) or (used_model not in df['model'][df['image']==next_img].values):
         final_dict, color, feature_list, rest_features, p_color,col_s=fitCNN('app/static/images/'+next_img,used_model)
         df=df.append({'model':used_model,'image':next_img,'final_dict':final_dict,
@@ -68,12 +80,14 @@ def update_img(next_img):
         p_color=df_part['p_color'].values[0]
         col_s=df_part['col_s'].values[0]
 
+    # Features selection, new image
     if request.method == 'POST':
+        # Select features from rest features, not in main feature list
         select = request.form.get('rest_f') 
         if select is not None:
             feature_list.append(select)
             rest_features.remove(select)
-
+        # Upload new image
         if 'file' not in request.files:
             flash('No file part')
         else:
@@ -86,17 +100,14 @@ def update_img(next_img):
                 img.insert(0, filename)
                 return redirect(url_for('index'))
 
+    # Save all
     df.to_pickle('app/static/cache/model_result.csv')
 
-
-    #Helpers for scroling through images
-
+    #Helpers for scroling through images, define next and previous image
     img_name='images/'+next_img
-
     idx=img.index(next_img)+1
     idx= idx if idx<len(img) else 0
     img_next=img[idx]
-
     idx=img.index(next_img)-1
     idx= idx if idx>=0 else len(img)-1
     img_prev=img[idx]
@@ -106,11 +117,10 @@ def update_img(next_img):
     for file in os.listdir("app/static/models"):
         if file.endswith(".h5"):
             models.append(file)
-
-    
+    # Do not show model in title
     models.remove(used_model)
 
-#----------------------Return all
+#----------------------Render web page
     return render_template("index.html",
                             img=url_for('static', filename = img_name),
                             data=feature_list,
@@ -123,10 +133,16 @@ def update_img(next_img):
                             col_s=col_s,
                             final_dict=final_dict)
 
-
-#To test pages
+################################SANBOX#######################
+#I dont want to break what works already. To test pages:
 @app.route('/test', methods=['GET', 'POST'])
 def test():
+    '''
+    Does whatever I want to test
+    input: nothing
+    output: nothing
+    '''
+
     cc=['50% - Hemd', '30% - Kleid','1% - Jacke']
     cat_2_sel=cc[0]
     if request.method == 'POST':
@@ -139,8 +155,14 @@ def test():
 
     return render_template("test.html",cat_2=cc,cat_2_sel=cat_2_sel)
 
+#################################CNN CLASSIFICATION####################
 #Helper to calculate probabilities from the model
 def fitCNN(image_path,modelname):
+    '''
+    Function fits CNN model to predict image category and features
+    input: path to image, should be smth like statec/images/name.png
+    output: predictions
+    '''
     #from app import best_model as model
     print('[INFO]: Loading data....')
     #read supporting csvs
@@ -148,7 +170,6 @@ def fitCNN(image_path,modelname):
     categories=pd.read_csv('app/static/models/categories_'+modelname[:-3]+'.csv')
     colors=pd.read_csv('app/static/models/colors.csv')
     feature_dict=pd.read_csv('app/static/models/rename_features.csv',sep=';')
-
 
     #Rename some encoded things
     categories_dict={ 'blusentuniken':'blusen, tuniken',
@@ -160,7 +181,6 @@ def fitCNN(image_path,modelname):
                      }
     categories['categories']=categories['categories'].replace(categories_dict, regex=True)
     feature_dict=pd.Series(feature_dict['decoding'].values,index=feature_dict['features']).to_dict()
-    #feature_dict={r'(\b){}(\b)'.format(k):r'\1{}\2'.format(v) for k,v in feature_dict.items()}
     features['features']=features['features'].map(feature_dict)
     do_not_keep=['l1','l16','l18','l2']
     
@@ -222,7 +242,7 @@ def fitCNN(image_path,modelname):
         cat_1_prob=cat_1_prob[cat_1_prob['cat_1'].str.contains('|'.join(cat_2_prob['cat_1'].values.tolist()))]
     else:
         cat_3_prob=None
-    #Combine into big hierarchy dictionary
+    # Combine into big hierarchy dictionary
     final_dict={}
     for h1 in range(len(cat_1_prob)):
         col='white' if h1>0 else '#e6e6ff'
@@ -242,9 +262,6 @@ def fitCNN(image_path,modelname):
 
         final_dict[cat_1_prob['cat_1'].iloc[h1]]['subh']=subh2
 
-
-
-    print(final_dict)
     #----------------------work with features predictions
     feat_list=pred[1][0]
     #color
@@ -252,7 +269,7 @@ def fitCNN(image_path,modelname):
     p_col=features[pos_color]
     pos=p_color.argsort()[-1:][::-1]
     color='%0.0f%% - %s'%(np.max(p_color)*100,p_col[pos].tolist()[0])
-    #other features
+    # other features
     feature_list=['%0.0f%% - %s'%(x*100,y) for x,y in zip(feat_list[feat_list>=0.7],features[feat_list>=0.7])]
     rest_features=['%0.0f%% - %s'%(x*100,y) for x,y in zip(feat_list[feat_list<0.7],features[feat_list<0.7])]
 
@@ -265,7 +282,7 @@ def fitCNN(image_path,modelname):
             rest_features.remove(f)
         except: print('not there')
 
-    #Color from segmentation
+    # Color from segmentation
     img = Image.open(image_path)
 
     mask=np.array(img.split()[-1])
